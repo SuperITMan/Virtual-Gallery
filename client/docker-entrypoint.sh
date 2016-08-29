cbc9503c741ea03af34c6b6e0e91bdf52319edd1
SuperITMan <admin@superitman.com>
2016-08-29 20:25:53 +0200

    Updated Client site.

---
+++ client/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copy manifest.json for user preferences
+ls /tmp
+if [ -f /tmp/manifest.json ]; then
+        cp /tmp/manifest.json /usr/src/app/client/src/app/assets-base/manifest.json
+fi;
+
+# Build website
+npm run build:prod
+
+# Delete unused files
+mv /usr/src/app/client/dist/* /usr/src/app/dist && mv superstatic.json ../
+rm -R /usr/src/app/client
+
+cd /usr/src/app/dist
+
+# Launch superstatic server when container runs
+superstatic --config /usr/src/app/superstatic.json --port 443 --host 0.0.0.0
